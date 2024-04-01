93801cbd95135822ff57792b29e51178d95db389
Mariya <juno2@mail.ru>
2024-04-01 20:29:56 +0300

    Добавили рабочий код позволяющий ввести символы с клавиатуры, и обработать их алгоритмом

--- IK1/Program.cs
+++ IK1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static List<string> Result(string[] input_array){
+        List<string> ret_l = new List<string>();
+
+        // по очереди ищем что нам не подходит
+        foreach(string item in input_array)
+        {
+            if(item.Length <= 3){
+                ret_l.Add(item);
+            }
+        }
+        return ret_l;
+
+    }
+    static void Main()
+    {
+
+        Console.Write("Введите элементы массива через запятую: ");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(',');
+
+        //[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+        //[“Russia”, “Denmark”, “Kazan”] → []
+
+        Console.WriteLine(string.Join(",", Result(inputArray)));
+    }
+}
+
+
